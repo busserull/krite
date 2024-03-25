2c7a72615ec1b2857d99a85962eeb77176d1153b
Kolbjørn Austreng <kolbjorn.austreng@hotzen.io>
2024-03-25 19:53:42 +0100

    course: js commands 2/2
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@hotzen.io>

--- lib/krite_web/live/shop/shop_live.ex
+++ lib/krite_web/live/shop/shop_live.ex
@@ -14,6 +14,16 @@ defmodule KriteWeb.ShopLive do
   end
 
   def toggle_cart do
-    JS.toggle(to: "#cart") |> JS.toggle(to: "#backdrop")
+    JS.toggle(
+      to: "#cart",
+      in: {"ease-in-out duration-300", "translate-x-full", "translate-x-0"},
+      out: {
+        "ease-in-out duration-300",
+        "translate-x-0",
+        "translate-x-full"
+      },
+      time: 300
+    )
+    |> JS.toggle(to: "#backdrop", in: "fade-in", out: "fade-out")
   end
 end
