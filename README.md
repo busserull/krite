13927f1ee7ac9002c1063d1b774c19366e47308d
Kolbjørn Austreng <kolbjorn.austreng@radionor.no>
2025-01-24 15:04:06 +0100

    create new balance and sauna cards
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@radionor.no>

--- README.md
+++ README.md
@@ -34,6 +34,10 @@ Ready to run in production? Please [check our deployment guides](https://hexdocs
 [x] Persist purchases to the database.
 [x] Make `Krite.Products.list_active_items` only return active items.
 [x] Highlight search match when searching in the shop.
+[x] Create new cards showing account balance and sauna pass at a glance.
+[ ] Refactor the back link component.
+[ ] Add a route explaining how to add money to Dypet.
+[ ] Refactor action elements.
 [ ] Add bar code scanning to devices that support it.
 [ ] Create a header where login information can be placed.
 [ ] Send dummy email with password reset links.
--- lib/krite_web/controllers/kveg_html.ex
+++ lib/krite_web/controllers/kveg_html.ex
@@ -5,6 +5,55 @@ defmodule KriteWeb.KvegHTML do
 
   attr(:balance, :integer, required: true)
 
+  # <div class="bg-gradient-to-br from-green-500 to-teal-600 rounded-md pt-3 px-5 relative overflow-hidden shadow-lg shadow-black/40 w-full flex flex-col justify-between items-start">
+
+  defp balance_card(assigns) do
+    ~H"""
+    <div class={["bg-gradient-to-br rounded-md pt-3 px-5 relative overflow-hidden shadow-lg shadow-black/40 w-full flex flex-col justify-between items-start",
+    @balance < 0 && "from-amber-500 to-pink-600" || "from-green-500 to-teal-600"]}>
+    <div>
+    <p class={["font-semibold", @balance < 0 && "text-amber-900" || "text-green-800"]}>Your account</p>
+    <p class={["font-semibold mt-1 mb-4", @balance < 0 && "text-amber-50" || "text-green-50"]}>
+      <span class="text-6xl"><%= @balance %></span> <span class="text-2xl">kr</span>
+    </p>
+    </div>
+
+    <div class={["border-t w-full hover:cursor-pointer", @balance < 0 && "border-amber-50/60" || "border-green-50/30"]}>
+    <p class={["font-semibold text-center pt-2", @balance < 0 && "text-amber-50 text-xl pb-1" || "text-green-800 pb-3"]}>
+      Click here to top it up
+    </p>
+    <p :if={@balance < 0} class="text-red-900 text-center text-semibold pb-3">
+      (You should do that now)
+    </p>
+    </div>
+    </div>
+    """
+  end
+
+  attr(:pass_end, :any, required: true)
+
+  defp sauna_card(assigns) do
+    ~H"""
+    <div class="bg-gradient-to-br from-green-500 to-teal-600 rounded-md py-4 px-5 relative z-10 overflow-hidden shadow-lg shadow-black/40 w-full flex flex-col justify-between items-start">
+    <img src={~p"/images/sauna_heat_lines.svg"} class="h-28 w-28 absolute -z-10 opacity-80 right-1 top-4" />
+
+    <div>
+    <p class="text-green-800 font-semibold">Sauna pass</p>
+    <p class="text-4xl font-semibold tracking-wider text-green-50 mt-1">Valid</p>
+    </div>
+
+    <div>
+    <p class="text-green-800 font-semibold mt-2">Expires</p>
+    <p class="text-2xl font-semibold tracking-wide text-green-50">
+      <%= format_expire_date(@pass_end) %>
+    </p>
+    </div>
+    </div>
+    """
+  end
+
+  attr(:balance, :integer, required: true)
+
   defp money_card(assigns) do
     ~H"""
     <div class={[
@@ -82,5 +131,29 @@ defmodule KriteWeb.KvegHTML do
       <%= render_slot(@inner_block) %>
     </a>
     """
+  end
+
+  defp format_expire_date(date) do
+    now = NaiveDateTime.utc_now()
+
+    cond do
+      NaiveDateTime.diff(date, now, :minute) < 5 * 60 ->
+        "Later today"
+
+      NaiveDateTime.diff(date, now, :hour) < 24 ->
+        "Tomorrow"
+
+      true ->
+        Calendar.strftime(date, "%B %-d") <> ordinal(date.day) <> " #{date.year}"
+    end
+  end
+
+  defp ordinal(number) do
+    case number do
+      1 -> "st"
+      2 -> "nd"
+      3 -> "rd"
+      _ -> "th"
+    end
   end
 end
